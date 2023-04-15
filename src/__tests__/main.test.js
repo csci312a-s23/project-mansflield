c8f30fb2b3d463bfdb9c3d71321b4c75dcce8f69
doabell <35297086+doabell@users.noreply.github.com>
2023-04-15 03:08:55 -0400

    test: mock router and fetch

--- src/__tests__/main.test.js
+++ src/__tests__/main.test.js
@@ -1,8 +1,27 @@
 import Home from "@/pages/index";
+import PlacePage from "@/pages/place/[id]";
 import { render } from "@testing-library/react";
+import mockRouter from "next-router-mock";
+
+import { createDynamicRouteParser } from "next-router-mock/dynamic-routes";
+mockRouter.useParser(
+  createDynamicRouteParser([
+    // These paths should match those found in the `/pages` folder
+    "/place/[id]",
+  ])
+);
+
+require("jest-fetch-mock").enableMocks();
+
+jest.mock("next/router", () => require("next-router-mock"));
 
 describe("End-to-end testing", () => {
   test("Render index.js component", () => {
     render(<Home />);
   });
+  test("Render page for Proctor", () => {
+    mockRouter.push("/place/proctor/");
+    render(<PlacePage />);
+  });
+  //
 });
