3c3bced12b072adf77f7dcf4f18a04fdb16e59ec
doabell <35297086+doabell@users.noreply.github.com>
2023-04-28 15:10:13 -0400

    Revert "Added Background Image"
    This reverts commit bdcd8a633ce0c43d8758f1d9b59fe330f542a9fe.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,7 +5,6 @@ import theme from "../material/theme";
 import createEmotionCache from "../material/createEmotionCache";
 import { ThemeProvider } from "@mui/material/styles";
 import { CacheProvider } from "@emotion/react";
-import styles from "../styles/home.module.css";
 
 const clientSideEmotionCache = createEmotionCache();
 
@@ -15,53 +14,51 @@ export default function MainApp({
   emotionCache = clientSideEmotionCache,
 }) {
   return (
-    <div className={styles.backImage}>
-      <CacheProvider value={emotionCache}>
-        <Head>
-          <title>MealHow</title>
-          <link rel="icon" type="image/svg+xml" href="favicon.svg" />
-          <link rel="icon" type="image/png" href="favicon.png" />
-          <meta name="viewport" content="initial-scale=1, width=device-width" />
-        </Head>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <Component {...pageProps} />
-          <footer>
-            <Stack
-              direction="row"
-              justifyContent="center"
-              alignItems="center"
-              spacing={2}
-              mt={2}
-              mb={{ xs: 20, sm: 2 }}
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <title>MealHow</title>
+        <link rel="icon" type="image/svg+xml" href="favicon.svg" />
+        <link rel="icon" type="image/png" href="favicon.png" />
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+        <footer>
+          <Stack
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+            spacing={2}
+            mt={2}
+            mb={{ xs: 20, sm: 2 }}
+          >
+            <Typography
+              variant="subtitle2"
+              color="text.secondary"
+              align="center"
             >
-              <Typography
-                variant="subtitle2"
-                color="text.secondary"
-                align="center"
+              {"We're no "}
+              <Link
+                color="inherit"
+                href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
               >
-                {"We're no "}
-                <Link
-                  color="inherit"
-                  href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-                >
-                  strangers to love
-                </Link>
-                {"."}
-                <br />
-                {"A "}
-                <Link
-                  color="inherit"
-                  href="https://www.cs.middlebury.edu/~mlinderman/courses/cs312/s23/index.html"
-                >
-                  CS 312
-                </Link>
-                {" Project"}
-              </Typography>
-            </Stack>
-          </footer>
-        </ThemeProvider>
-      </CacheProvider>
-    </div>
+                strangers to love
+              </Link>
+              {"."}
+              <br />
+              {"A "}
+              <Link
+                color="inherit"
+                href="https://www.cs.middlebury.edu/~mlinderman/courses/cs312/s23/index.html"
+              >
+                CS 312
+              </Link>
+              {" Project"}
+            </Typography>
+          </Stack>
+        </footer>
+      </ThemeProvider>
+    </CacheProvider>
   );
 }
