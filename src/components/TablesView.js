e468478e9262729adf201deea806f3cafb9651ea
Sirui <siruic@middlebury.edu>
2023-05-07 22:58:39 -0400

    feat02NewTableViews

--- src/components/TablesView.js
+++ src/components/TablesView.js
@@ -3,29 +3,57 @@
 
   This component provides the how many tables text and slider in individial dining hall pages.
 */
-
+import * as React from "react";
 import { useState } from "react";
+import { Button, Slider, Stack, Typography } from "@mui/material";
 import PropTypes from "prop-types";
 
 export default function TablesView({ hall, info }) {
   const [value, setValue] = useState(info.tablesVal);
-  const onChange = (event) => {
+
+  function Tvaluetext(Tvalue) {
+    const TableAmount = [
+      "Completely Empty",
+      "Many Tables",
+      "A Few Tables Left",
+      "Barely Find One",
+      "No Tables Left",
+    ];
+    const TableIndex = Tvalue;
+    return `${TableAmount[TableIndex]}`;
+  }
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const SubmitChange = (event) => {
     setValue(parseInt(event.target.value));
   };
 
   return (
     <>
-      <p>
+      <Typography>
         Tables at {hall.name}: {info.tables}
-      </p>
-      <input
-        type="range"
-        min="0"
-        max="4"
-        value={value}
-        className="slider"
-        onChange={onChange}
-      />
+      </Typography>
+      <Stack direction="row" spacing={2}>
+        <Slider
+          value={value}
+          defaultValue={2}
+          step={1}
+          marks
+          min={0}
+          max={4}
+          getAriaValueText={Tvaluetext}
+          valueLabelFormat={Tvaluetext}
+          valueLabelDisplay="auto"
+          aria-labelledby="non-linear-slider2"
+          onChange={handleChange} // Added the onChange event handler
+        />
+        <Button onClick={SubmitChange} type="button">
+          Submit
+        </Button>
+      </Stack>
     </>
   );
 }
